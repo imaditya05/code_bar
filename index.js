ed9fa6388edb0d1e40d7e3130b9df3657ebcec6e
Aditya <someone@someplace.com>
2022-12-21 15:56:58 +0530

    partials and layout same and final

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// Extract styles and script from sub pages into the layout
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // use express router 
 
@@ -8,8 +18,8 @@ app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.listen(port, function(err){
-    if(err){
+app.listen(port, function (err) {
+    if (err) {
         console.log(`Error in running the server : ${err}`); // interpolation 
     }
     console.log(`Server is running on port: ${port}`);
