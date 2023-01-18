ee196000bb2041a4fb460e8df450412a7224fc6d
Aditya <someone@someplace.com>
2023-01-18 23:26:43 +0530

    Converted the the code to Async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,38 +1,27 @@
-const { populate } = require('../models/post');
-const Post = require('../models/post');
-const User = require('../models/user');
+const { populate } = require("../models/post");
+const Post = require("../models/post");
+const User = require("../models/user");
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('userid', 25);
-    
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Code Bar | Home",
-    //         posts: posts
-    //     })
-    // });
-
-    //Populate the user of each post 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
+module.exports.home = async function (req, res) {
+  //Populate the user of each post
+  try {
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
         populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+          path: "user",
+        },
+      });
 
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Code Bar | Home",
-                posts: posts,
-                all_users: users
-            });
-        });
+    let users = await User.find({});
 
-        
-    })
-    }
-
+    return res.render("home", {
+      title: "Code Bar | Home",
+      posts: posts,
+      all_users: users,
+    });
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
